1aea045448662eababf8c8e797741b185f39c9a0
João Vitor Prado <joaopradovitor99@gmail.com>
2025-06-30 20:11:56 -0300

    Commit Proj2

---
+++ Faculdade/Semestre 2/Front-End/Atv-Projeto2/script.js
@@ -0,0 +1,17 @@
+function acessarFormulario() {
+  setTimeout(() => {
+    window.open("Exemplo1-Formulario/index.html", "_blank")
+  }, 1000)
+}
+
+function acessarTodoList() {
+  setTimeout(() => {
+    window.open("Exemplo2-TodoList/index.html", "_blank")
+  }, 1000)
+}
+
+function acessarJogo() {
+  setTimeout(() => {
+    window.open("Exemplo3-Jogo/index.html", "_blank")
+  }, 1000)
+}
